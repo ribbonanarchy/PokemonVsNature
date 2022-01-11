751378f5da7cd0532c265b462ad53d9951bdd7e5
ByrdBassPC <byrdbass@gmail.com>
2022-01-11 10:10:06 -0600

    added arrays to pokemon API

--- Lelandspokemonapi.js
+++ Lelandspokemonapi.js
@@ -1,12 +1,28 @@
-var url = 'https://pokeapi.co/api/v2/' + endpoint +'/' + idOrName
-var endpoint = 'pokemon'
-var idOrName = 35;
+//var url = 'https://pokeapi.co/api/v2/' + endpoint +'/' + idOrName
+var endpoint = [
+    'pokemon', 
+    'type',
+    'move-damage-class',
+    'move-category',
+]
+var idOrName =[
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+]
+var url = 'https://pokeapi.co/api/v2/' + endpoint[0] + '/' + idOrName[0] + '/' // + "sprites/front_default"
+
+
 
 function getApi() {
     fetch (url)
-    console.log(response)
-    .then(function (response){
-        
+    .then(function (response) {
+        console.log('hello')
+        console.log(response)
         return response.json();
     })
+    
 }
+getApi();
