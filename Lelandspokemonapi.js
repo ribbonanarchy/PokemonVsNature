ea92ee85e80a6da8eff48159ef63f5549a812e29
ByrdBassPC <byrdbass@gmail.com>
2022-01-12 11:59:52 -0600

    started building switch case for status Effects and PokemonTypes

--- Lelandspokemonapi.js
+++ Lelandspokemonapi.js
@@ -1,31 +1,109 @@
 //var url = 'https://pokeapi.co/api/v2/' + endpoint +'/' + idOrName
+var pokemonTypes = [
+    {type: 'normal', //0
+    power: 100},
+    {type: 'fighting', //1
+    Power: 100},
+    {type: 'flying', //2
+    power: 100}, 
+    {type: 'poison', //3
+    power: 100},
+    {type: 'ground', //4
+    power: 100},
+    {type: 'rock', //5
+    power: 100},
+    {type: 'bug', //6
+    power: 100},
+    {type: 'ghost', //7
+    power: 100},
+    {type: 'steel', //8
+    power: 100},
+    {type: 'fire', //9
+    Power: 100},
+    {type: 'water', //10
+    Power: 100},
+    {type: 'grass', //11
+    Power: 100},
+    {type: 'electric', //12
+    power: 100},
+    {type: 'dragon', //13
+    Power: 100},
+    {type: 'dark', //14
+    Power: 100},
+    {type: 'fairy', //15
+    Power: 100},
+];
+// for (i=0; i<pokemonTypes.length; i++) {
+// console.log("the pokemon type array is " + pokemonTypes[i].type);
+// };
+// console.log("the pokemon type array length is " + pokemonTypes.length);
+var pokemonDefaultPower = 100;
+var pokemonNegativeStatus = pokemonDefaultPower-50;
+var pokemonPositiveStatus = pokemonDefaultPower+50;
+// console.log("the positive effect is " + pokemonPositiveStatus);
+// console.log("the negative effect is " + pokemonNegativeStatus);
+var weatherTypes = [
+    'clearSky', 
+    // flying+[2], dragon+[13] , fire+[9], dark-[14], ghost-[7], bug-[6], water-[10]
+    'cloudy',
+    //poison+, dark+, fighting+, normal-
+    'partlyCloudy',
+    //poison+, dark+, fighting+, bug+
+    'rain',
+    //water+, grass+, ground+, rock-, steel-, fire-, poison-
+    'thunderstorm',
+    //electric+, dragon-, flying-, fire-, rock-, normal+
+    'snow',
+    //ground-, normal+, ghost+, rock+, steel+
+    'mist'
+    //fairy+, fighting-, ghost+, bug+, electric-
+];
+//console.log("this is the array of weatherTypes " + weatherTypes)
+//console.log("this is the first Index of weatherTypes " + weatherTypes[0])
+
+var currentWeather = weatherTypes[0];
+console.log("the current weather is " + currentWeather)
+/*I will need to associate each pokemonType with a starting power of 'pokemonDefaultPower'
+then loop through each type of pokemon?
+I will need if statements drawn up for each pokemon type and weathertype*/
+switch (currentWeather) {
+    case weatherTypes[0]: 
+        pokemonTypes[2].power + pokemonPositiveStatus, 
+        pokemonTypes[13].power + pokemonPositiveStatus,
+        pokemonTypes[9].power + pokemonPositiveStatus;
+        console.log(pokemonTypes[2].power)
+        
+    break;
+    case weatherTypes[1]: pokemonTypes[2].power - pokemonNegativeStatus;
+    break;
+    case weatherTypes[2]: pokemonTypes[2].power - pokemonNegativeStatus;
+    break;
+    case weatherTypes[3]: pokemonTypes[2].power - pokemonNegativeStatus;
+    break;
+    case weatherTypes[4]: pokemonTypes[2].power - pokemonNegativeStatus;
+    break;
+    case weatherTypes[5]: pokemonTypes[2].power - pokemonNegativeStatus;
+    break;
+}
+
+console.log("the pokemon type " + pokemonTypes[0].type + " will have a negative status with " + weatherTypes[1])
+
+
+// var pokemonStatusEffects [
+
+// ]
+
+//create an equation for the types of weather associated with pokemon Type
+
 var endpoint = [
     'pokemon', 
     'type',
     'move-damage-class',
     'move-category',
 ]
-var weatherTypes = [
-    'clearSky', 
-    'cloudy',
-    'partlyCloudy',
-    'rain',
-    'thunderstorm',
-    'snow',
-    'mist'
-];
 
-var pokemonTypes = [
-    'normal',
-    'fighting',
-    'flying',
-    'poison',
-    'ground',
-    'rock',
-    'bug',
-    'ghost',
-    'steel'
-];
+
+
 
 /*//types of weather
 var clearSky
@@ -57,12 +135,12 @@ var url = 'https://pokeapi.co/api/v2/' + endpoint[0] + '/' + idOrName + '/' // +
 function getApi() {
     fetch (url)
     .then(function (response) {
-        console.log('hello')
+        console.log('hello this is the response function')
         console.log(response)
         return response.json();
     })
     .then( data => {
-        console.log('hello')
+        console.log('hello this is the data function')
         console.log(data);
         
         //shows name of pokemon
