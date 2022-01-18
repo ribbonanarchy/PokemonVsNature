3b2ae83ecaa17a92a452ad38bb7ae7fb081e1ecb
Robyn Arnecke <robynarnecke@gmail.com>
2022-01-18 14:20:48 -0600

    Got the user's 6 pokemon array to populate and one pokemon to show on screen

--- assets/scriptFromRobyn.js
+++ assets/scriptFromRobyn.js
@@ -93,7 +93,7 @@ var weatherTypes = [
     //poison+[3], dark+[14], fighting+[1], normal-[0] 
     },
     {type: 'partlyCloudy', //[2]
-    positiveIndex: [3,14,1,6]
+    positiveIndex: [3,14,1]
     //poison+[3], dark+[14], fighting+[1], bug+[6]
     },
     {type: 'Rain',//[3]
@@ -102,12 +102,12 @@ var weatherTypes = [
     //water+[10], grass+[11], ground+[4], rock-[5], steel-[8], fire-[9], poison-[3]
     },
     {type: 'thunderstorm',//[4]
-    positiveIndex: [12,0],
+    positiveIndex: [12,0,1],
     negativeIndex: [13,2,9,5]
     //electric+[12], normal+[0], dragon-[13], flying-[2], fire-[9], rock-[5], 
     },
     {type: 'Snow',//[5]
-    positiveIndex: [0,7,5,8],
+    positiveIndex: [0,7,5],
     negativeIndex: [4]
     //normal+[0], ghost+[7], rock+[5], steel+[8], ground-[4], 
     },
@@ -120,8 +120,8 @@ var weatherTypes = [
 
 //console.log(pokemonTypes[4].pokemonArray);
 var pokeArray;
-var typeMasterArray; //hold all the pokemon affected by current weather
-var userPokeChoice; //pick 6 random from typeMasterArray
+var typeMasterArray = []; //hold all the pokemon affected by current weather
+var userPokeChoice = []; //pick 6 random from typeMasterArray
 var currentWeather, currentPlus, currentNegative, currentWeatherIndex;
 
 // ATTEMPT TO LOAD JAVASCRIPT DATA FROM EXTERNAL SOURCE - ASK PROF?!!
@@ -215,8 +215,8 @@ function getForecastUvi (lat,lon) {
           //console.log(data.daily[0].weather[0].main);
           $(".pokeCard").removeClass("hide");
           var day0Weather = data.daily[0].weather[0].main;
-        //   weatherToPokemon(day0Weather);
-            currentWeather = day0Weather;
+          weatherToPokemon(day0Weather);
+            console.log("Day 0: " + day0Weather);
         })
         $('#day1Card').click(function() {
           //console.log("hello this is the day1Card")
@@ -224,8 +224,8 @@ function getForecastUvi (lat,lon) {
           //console.log(data.daily[1].weather[0].main);
           $(".pokeCard").removeClass("hide");
           var day1Weather = data.daily[1].weather[0].main;
-        //   weatherToPokemon(day1Weather);
-            currentWeather = day1Weather;
+          weatherToPokemon(day1Weather);
+            console.log("Day 1: " + day1Weather);
         })
         $('#day2Card').click(function() {
           //console.log("hello this is the day2Card")
@@ -233,8 +233,8 @@ function getForecastUvi (lat,lon) {
           //console.log(data.daily[2].weather[0].main);
           $(".pokeCard").removeClass("hide");
           var day2Weather = data.daily[2].weather[0].main;
-        //   weatherToPokemon(day2Weather);
-            currentWeather = day2Weather;
+          weatherToPokemon(day2Weather);
+            console.log("Day 2: " + day2Weather);
         })
         $('#day3Card').click(function() {
           //console.log("hello this is the day3Card")
@@ -242,8 +242,8 @@ function getForecastUvi (lat,lon) {
           //console.log(data.daily[3].weather[0].main);
           $(".pokeCard").removeClass("hide");
           var day3Weather = data.daily[3].weather[0].main;
-        //   weatherToPokemon(day3Weather);
-            currentWeather = day3Weather;
+          weatherToPokemon(day3Weather);
+            console.log("Day 3: " + day3Weather);
         })
         $('#day4Card').click(function() {
           //console.log("hello this is the day4Card")
@@ -251,15 +251,22 @@ function getForecastUvi (lat,lon) {
           //console.log(data.daily[4].weather[0].main);
           $(".pokeCard").removeClass("hide");
           var day4Weather = data.daily[4].weather[0].main;
-        //   weatherToPokemon(day4Weather);
-            currentWeather = day4Weather;
+          weatherToPokemon(day4Weather);
+            console.log("Day 4: " + day4Weather);
         })
-        return currentWeather;
+        
+            return currentWeather;
         })
-        .then(function(currentWeather) {
-            console.log(currentWeather);
+        .then(function(typeMasterArray) {
+           
         })
+        
 }
+
+
+
+// console.log(currentWeather);
+
 function cityListPopulate () {
     cityName = $('#autocomplete-input').val().trim();
     //console.log('hello this is the cityListPopulate function');
@@ -301,13 +308,17 @@ function weatherToPokemon (day0Weather, day1Weather, day2Weather, day3Weather, d
     currentWeather = day0Weather || day1Weather || day2Weather || day3Weather || day4Weather;
     console.log("the current weather is " + currentWeather);
     
-    // populate arrays for each type
-    for(var i=0; i<pokemonTypes.length; i++) {
-    //    pokemonTypes[i].pokemonArray = getTypeAPI(i, currentWeather);
+    currentWeatherIndex = weatherTypes.findIndex(weather => weather.type === currentWeather);
+    // console.log(currentWeatherIndex);
+    // populate arrays for each type that is affected by current weather
+    for(var i=0; i<weatherTypes[currentWeatherIndex].positiveIndex.length; i++) {
+       getTypeAPI(weatherTypes[currentWeatherIndex].positiveIndex[i], currentWeather);
     }
+
+    
     console.log(currentWeather);
 }
-weatherToPokemon();
+// weatherToPokemon();
 
 
 
@@ -318,13 +329,13 @@ function getTypeAPI(i, currentWeather) {
     })
     .then(data => {
         pokeArray = data.pokemon.slice(0,10);
-        console.log(data.pokemon)
+        // console.log(data.pokemon);
         return pokeArray;
     })
     .then(function(pokeArray) {
         pokemonTypes[i].pokemonArray = pokeArray;
         console.log('Here are ten ' + pokemonTypes[i].type + ' pokemon: ');
-        console.log('this is the pokemon array' + pokemonTypes[i].pokemonArray);
+        console.log(pokemonTypes[i].pokemonArray);
         return pokemonTypes;
     })
     .then(function(pokemonTypes) {
@@ -334,18 +345,47 @@ function getTypeAPI(i, currentWeather) {
         console.log('the current weather index is ' + currentWeatherIndex);
         console.log('the positive index is ' + currentPlus)
         console.log('the negative index is ' + currentNegative)
-        for (j=0; j<currentPlus.length; j++) {
-        console.log('this is an example of the pokemon effected positively ' + pokemonTypes[j].id)
-        };
+        // for (j=0; j<currentPlus.length; j++) {
+        // console.log('this is an example of the pokemon effected positively ' + pokemonTypes[j].id);
+        // };
+        
+        typeMasterArrayPopulate(currentPlus, currentNegative);
+        
+        console.log(typeMasterArray);
+
+        return typeMasterArray;
+        
+    })
+    .then(function(typeMasterArray) {
+        var randomPosition1 = Math.floor(Math.random() * 9);
+        var randomPosition2 = Math.floor(Math.random() * 9);
+        userPokeChoice.push(typeMasterArray[6][randomPosition1]);
+        userPokeChoice.push(typeMasterArray[6][randomPosition2]);
+        userPokeChoice.push(typeMasterArray[7][randomPosition1]);
+        userPokeChoice.push(typeMasterArray[7][randomPosition2]);
+        userPokeChoice.push(typeMasterArray[8][randomPosition1]);
+        userPokeChoice.push(typeMasterArray[8][randomPosition2]);
+
+        console.log(userPokeChoice);
+        return userPokeChoice;
+    })
+    .then(function(userPokeChoice) {
+        for(var i=0; i<userPokeChoice.length; i++) {
+            getOnePokemon(userPokeChoice[i].pokemon.url);
+            console.log(userPokeChoice[i].pokemon.url);
+        }
     });
 }
 
 
-// function assignArrays(data) {
-//     pokeArray = data.pokemon;
-//     pokemonTypes[i].pokemonArray = pokeArray.slice(0,10);
-// }
 
+function typeMasterArrayPopulate(currentPlus, currentNegative) {
+    for(var i=0; i<currentPlus.length; i++) {
+        typeMasterArray.push(pokemonTypes[currentPlus[i]].pokemonArray);
+    }
+
+    return typeMasterArray;
+}
 
 
 // for (i=0; i<pokemonTypes.length; i++) {
@@ -441,50 +481,34 @@ var pokeFrontImage = document.querySelector('.poke-front-image');
 console.log(pokeName);
 
 //for (i=0; i<idOrName.length; i++) {
-// var urlSinglePoke = 'https://pokeapi.co/api/v2/' + endpoint[0] + '/' + idOrName + '/' // + "sprites/front_default"
-// function getOnePokemon() {
-//     fetch (urlSinglePoke)
-//     .then(function (response) {
-//         console.log('hello this is the response function for urlSinglePoke')
-//         console.log(response)
-//         return response.json();
-//     })
-//     .then( data => {
-//         console.log('hello this is the data function for urlSinglePoke')
-//         console.log(data);
+// var urlSinglePoke = 'https://pokeapi.co/api/v2/pokemon/' + id + '/' // + "sprites/front_default"
+function getOnePokemon(url) {
+    fetch (url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then( data => {
+        console.log('hello this is the data function for urlSinglePoke');
+        console.log(data);
         
-//         //shows name of pokemon
-//         console.log(data['name']);
-//         pokeName.textContent = (data['name'])
+        //shows name of pokemon
+        console.log(data['name']);
+        pokeName.textContent = (data['name']);
 
-//         //pokemon types variables
-//         var dataTypes = data['types'];
-//         var dataFirstType = dataTypes[0];
-//         var dataSecondType = dataTypes[1];
+        var dataTypes = data['types'];
+        var dataFirstType = dataTypes[0];
+        //shows 1st type of pokemon i.e 'grass/water/poison'
+        console.log(dataFirstType['type']['name']);
+        pokeTypeOne.textContent = (dataFirstType)['type']['name'];
+        
+        //images for pokemon
+        pokeFrontImage.src = data['sprites']['front_default'];
 
-//         //shows 1st type of pokemon i.e 'grass/water/poison'
-//         console.log(dataFirstType['type']['name']);
-//         pokeTypeOne.textContent = (dataFirstType)['type']['name'];
-//         //creates variables to check if there are more than one type for each pokemon
-//         if (dataSecondType) {
-//             pokeTypeTwo.classList.remove('hide')
-//             pokeTypeTwo.textContent = (dataSecondType)['type']['name'];
-//             console.log = (dataSecondType['type']['name']);
-
-//         } else {
-//             pokeTypeTwo.classList.add('hide');
-//             pokeTypeTwo.textContent = '';
-//             //empty string to display nothing
-//             console.log = ("this pokemon has no second type");
-//         }
-
-//         //images for pokemon
-//         pokeFrontImage.src = data['sprites']['front_default'];
-//         //display the color for the type of pokemon!!
-//         pokeCard.classList.add((dataFirstType)['type']['name']);
-//     })
-// }
-// //}
+        //display the color for the type of pokemon!!
+        pokeCard.classList.add((dataFirstType)['type']['name']);
+    })
+}
+//}
 // getOnePokemon();
 
 
